8f6359591bb5e7c47cc20109631ed0852d8a07bb
Thomaz Alves <thomazhilario5@gmail.com>
2026-05-06 12:15:41 +0000

    Refactor: FormFieldUploadFile component style and logic.

--- refund-frontend/src/Components/Form/FormFieldUploadFile.tsx
+++ refund-frontend/src/Components/Form/FormFieldUploadFile.tsx
@@ -1,7 +1,10 @@
+import { Fragment, useRef, type ChangeEvent } from "react";
 import { useController, useFormContext, type FieldPath, type FieldValues } from "react-hook-form"
-import { cn } from "../../utils";
+
 import { FormFieldMessageError } from "./FormFieldMessageError";
-import type { ChangeEvent } from "react";
+import { UploadIcon } from "../Icons";
+
+import { cn } from "../../utils";
 
 type FormFieldUploadFileProps<TFieldValues extends FieldValues> = {
     id: string;
@@ -25,6 +28,8 @@ export const FormFieldUploadFile = <TFieldValues extends FieldValues>({
         name,
     })
 
+    const inputRef = useRef<HTMLInputElement>(null)
+
     const handleUploadFile = (e: ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0]
 
@@ -33,24 +38,40 @@ export const FormFieldUploadFile = <TFieldValues extends FieldValues>({
         field.onChange(file)
     }
 
+    const handleOpenFile = () => {
+        const file = inputRef.current
+
+        if(!file) return
+
+        file.click()
+    }
+
     return (
-        <label className={cn(className)} htmlFor={id}>
-            <span className="mb-2 block text-xs font-medium text-gray-700">
-                {label}
-            </span>
+        <Fragment>
+            <input
+                ref={inputRef}
+                type="file"
+                onChange={handleUploadFile}
+                hidden
+            />
 
-            <div>
-                <input
-                    placeholder={placeholder}
-                    className={cn('w-full rounded-md border-2 border-gray-300 p-4')}
-                    type="file"
-                    onChange={handleUploadFile}
-                />
-                
-                <button></button>
+            <div className="flex flex-col gap-4">
+                <span className="block text-xs font-medium text-gray-700">
+                    {label}
+                </span>
+
+                <label htmlFor={id} className={cn('flex justify-between gap-4', className)}>
+                    <span className={cn('w-full rounded-md border-2 border-gray-300 p-2.5 pointer-events-none')}>{field.value?.name ?? placeholder}</span>
+
+                    <div className="flex justify-between items-center gap-4">
+                        <button type="button" onClick={handleOpenFile} className="bg-button w-16 h-12 rounded-md flex items-center justify-center cursor-pointer">
+                            <UploadIcon className="text-white pointer-events-none"/>
+                        </button>
+                    </div>
+                </label>
+
+                <FormFieldMessageError nameField={name} />
             </div>
-
-            <FormFieldMessageError nameField={name} />
-        </label>
+        </Fragment>
     )
 }
